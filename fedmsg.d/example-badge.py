22d37f6be47c9d0718d0e7d9752922749ca782a8
Ralph Bean <rbean@redhat.com>
2012-11-12 15:08:10 -0500

    Just some indenting..

--- fedmsg.d/example-badge.py
+++ fedmsg.d/example-badge.py
@@ -6,6 +6,6 @@ config = dict(
             badge_image='http://3.bp.blogspot.com/-XhjKweGVJHI/TZBrIJugrBI/AAAAAAAAAJM/ozRJi2bLAK4/s1600/fedora-logo.png',
             badge_desc='An Example Badge awarded for being an example',
             badge_criteria='http://fedoraproject.org'
-            ),
+        ),
     ]
 )
--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
 from setuptools import setup
 requires = [
-        'tahrir-api',
-        'fedmsg']
+    'tahrir-api',
+    'fedmsg',
+]
 
 long_description = file("README.rst").read().strip()
 setup(
-        name='fedbadges',
-        version='0.1.4',
-        description='fedmsg consumer for awarding open badges',
-        long_description=long_description,
-        author='Ross Delinger',
-        author_email='rdelinge@redhat.com',
-        url='https://github.com/rossdylan/fedbages',
-        install_requires=requires,
-        include_package_data=True,
-        packages=[
-            'fedbadges',
-            'fedbadges.consumers',
-            'fedbadges.commands'],
-        zip_safe=False,
-        entry_points={
-            'console_scripts': [
-                "fedmsg-badges=fedbadges.commands.badges:badges"],
-            'moksha.consumer': [
-                "fedmsg-badges=fedbadges.consumers.ExampleBadge:ExampleBadgesConsumer"]},)
+    name='fedbadges',
+    version='0.1.4',
+    description='fedmsg consumer for awarding open badges',
+    long_description=long_description,
+    author='Ross Delinger',
+    author_email='rdelinge@redhat.com',
+    url='https://github.com/rossdylan/fedbages',
+    install_requires=requires,
+    include_package_data=True,
+    packages=[
+        'fedbadges',
+        'fedbadges.consumers',
+        'fedbadges.commands'],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            "fedmsg-badges=fedbadges.commands.badges:badges",
+        ],
+        'moksha.consumer': [
+            "fedmsg-badges=fedbadges.consumers.ExampleBadge:ExampleBadgesConsumer",
+        ],
+    },
+)
